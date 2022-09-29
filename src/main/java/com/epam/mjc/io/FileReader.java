65c327f66f099e41e43ac46d95effa3bb20b7152
Nadzeya Petrova <nightmare.evans13@gmail.com>
2022-09-29 21:41:15 +0300

    Task IO

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,23 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+
+import java.io.*;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String[] profileData = new String[4];
+        String inString;
+        try (BufferedReader inStream = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
+
+            for (int i = 0; i < 4; i++) {
+                inString = inStream.readLine();
+                profileData[i] = inString.substring(inString.indexOf(':') + 2).trim();
+            }
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        return new Profile(profileData[0], Integer.parseInt(profileData[1]), profileData[2], Long.parseLong(profileData[3]));
     }
 }
